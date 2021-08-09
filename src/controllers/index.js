6de37d1ff791e67df0930cd72f4955fbe3f0ee50
Tajudeen Abdulgafar <66276321+Abdulgafar4@users.noreply.github.com>
2021-08-09 11:18:00 +0100

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,51 +1,89 @@
-import mongoose from 'mongoose';
-import { ProductSchema } from '../models/index';
+import mongoose from "mongoose";
+import { ProductSchema } from "../models/index";
 
-const Product = mongoose.model('Product', ProductSchema);
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "abdulgafar4",
+  api_key: "466349187453724",
+  api_secret: "QNOYvHf3b706dsGEtuoNryvaoYU",
+});
+
+const Product = mongoose.model("Product", ProductSchema);
 
 export const addNewProduct = (req, res) => {
-    let newProduct = new Product(req.body);
+  let newProduct = new Product(req.body);
 
-    newProduct.save((err, product) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json(product);
+  newProduct.save((err, product) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(product);
+  });
+  const data = {
+    productImage: req.body.productImage,
+  };
+
+  cloudinary.uploader
+    .upload(data.productImage, {
+      overwrite: true,
+      invalidate: true,
+      width: 810,
+      height: 456,
+      crop: "limit",
+    })
+    .then((res) => res.json())
+    .then((result) => {
+      response.status(200).send({
+        message: "success",
+        result,
+      });
+    })
+    .catch((error) => {
+      response.status(500).send({
+        message: "failure",
+        error,
+      });
     });
 };
 
 export const getProducts = (req, res) => {
-    Product.find({}, (err, product) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json(product);
-    });
+  Product.find({}, (err, product) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(product);
+  });
 };
 
 export const getProductWithID = (req, res) => {
-    Product.findById(req.params.productId, (err, product) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json(product);
-    });
-}
+  Product.findById(req.params.productId, (err, product) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(product);
+  });
+};
 
 export const updateProduct = (req, res) => {
-    Product.findOneAndUpdate({ _id: req.params.productId}, req.body, { new: true }, (err, product) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json(product);
-    })
-}
+  Product.findOneAndUpdate(
+    { _id: req.params.productId },
+    req.body,
+    { new: true },
+    (err, product) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(product);
+    }
+  );
+};
 
 export const deleteProduct = (req, res) => {
-    Product.remove({ _id: req.params.productId }, (err, product) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json({ message: 'Successfully deleted product'});
-    })
-}
+  Product.deleteOne({ _id: req.params.productId }, (err, product) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({ message: "Successfully deleted product" });
+  });
+};
