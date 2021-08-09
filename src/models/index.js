3806a0268357f73dca9ad7f745816dca83625d95
Tajudeen Abdulgafar <66276321+Abdulgafar4@users.noreply.github.com>
2021-08-09 11:18:54 +0100

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,40 +1,45 @@
-import mongoose from 'mongoose';
+"use strict";
 
-const Schema = mongoose.Schema
+import mongoose from "mongoose";
 
-export const ProductSchema = new Schema ({
-     productName: {
-        type: String,
-        required: 'Enter product name'
-     },
-     productCode: {
-        type: String,
-        required: 'Enter product code'
-     },
-     productImage: {
-        type: String,
-     },
-     quantity: {
-        type: Number,
-     },
-     price: {
-        type: String,
-     },
-     costPrice: {
-        type: String,
-     },
-     unit: {
-        type: String,
-     },
-     describtion: {
-        type: String,
-     },
-     tag: {
-        type: String,
-     },
-     created_Date: {
-        type: Date,
-        default: Date.now
-     },
+const mongooseDateFormat = require("mongoose-date-format");
 
-})
+const Schema = mongoose.Schema;
+
+export const ProductSchema = new Schema({
+  productName: {
+    type: String,
+    required: "Enter product name",
+  },
+  productCode: {
+    type: String,
+    required: "Enter product code",
+  },
+  productImage: {
+    type: String,
+  },
+  quantity: {
+    type: Number,
+  },
+  price: {
+    type: String,
+  },
+  costPrice: {
+    type: String,
+  },
+  unit: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  tag: {
+    type: String,
+  },
+  created_date: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+ProductSchema.plugin(mongooseDateFormat);
